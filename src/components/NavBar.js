8f1d7a774fdf632f8e046ba1c4f42410c7be0f73
joycewangui <joycewangui590@gmail.com>
2024-08-21 08:57:54 +0300

    Adds key tabs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  return (
+  <nav>{/* display an <a> tag for each link here */}
+    {links.map((link) => <a key = {link} href={`#${link}`}>{link}</a>)}
+  </nav>
+  );
+
+};
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,9 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */
+        projects.map((project) => <ProjectItem key={project.id} {...project} />)}
+      </div>
     </div>
   );
 }
